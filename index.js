eaa3ca0d3d50dbfa53902671c629b5f88b844527
berekt24 <bashenafi35@gmail.com>
2022-11-23 18:21:20 -0500

    questions and options

--- index.js
+++ index.js
@@ -34,4 +34,102 @@ const managerQuestions = () => {
     console.log(answers);
     const manager = new Manager(answers.name, answers.id, answers.email, answers.officNumber);
     theTeam.push(manager);
-})}
+    teamOptions();
+})};
+
+const teamOptions = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'userChoice',
+            message: 'What would you like to do?',
+            choices: [
+                'addEngineer',
+                'addIntern',
+                'finishedTeam'
+            ]
+        }
+    ])
+    .then((answer) => {
+        switch (answer.userChoice) {
+            case 'addEngineer':
+                addEngineer();
+                break;
+            case 'addIntern':
+                addIntern();
+                break;
+            default: 
+                createTeam();
+        } 
+    }) 
+}
+function addEngineer() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is your employee Id?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?',
+        },
+    ])
+
+.then(answers => {
+console.log(answers);
+const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+theTeam.push(engineer);
+teamOptions();
+})  
+}
+function addIntern() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is your employee Id?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is your school name?',
+        },
+    ])
+
+.then(answers => {
+console.log(answers);
+const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+theTeam.push(intern);
+teamOptions();
+})  
+}
+function createTeam() {
+    console.log(theTeam);
+    // to be completed
+}
+managerQuestions();
