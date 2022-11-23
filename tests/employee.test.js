9ccc3de62af8805cafde74c9bc92954471ba7e85
berekt24 <bashenafi35@gmail.com>
2022-11-22 20:43:07 -0500

    added manager consructor and tests to employee

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -10,3 +10,15 @@ it('should be able to get constructor values for Employee object', () => {
 it('should be able to get name from getName() method', () => {
     expect(employee.getName()).toBe('berekt');
 });
+
+it('should be able to get id from getId() method', () => {
+    expect(employee.getId()).toBe('981235');
+});
+
+it('should be able to get email from getEmail() method', () => {
+    expect(employee.getEmail()).toBe('bashenafi35@gmail.com');
+});
+
+it('should be able to get role from getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
